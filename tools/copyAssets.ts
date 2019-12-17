304733f89ecebac4d5dcec5db171879f1fe6c9c1
Darren Baines <darren.baines@netwealth.com>
2019-12-17 13:25:01 +0000

    Move build to webpack

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -2,3 +2,10 @@ import * as shell from "shelljs";
 
 // Copy all the view templates
 shell.cp( "-R", "src/views", "dist/" );
+shell.cp( "-R", "web.config", "dist/" );
+
+// Copy all the view templates
+shell.cp( "-R", "package*.json", "build/" );
+shell.cp( "-R", "src/views", "build/" );
+shell.cp( "-R", "logs", "build/" );
+shell.cp( "-R", "web.config", "build/" );
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const {
+  NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  externals: [ nodeExternals() ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  node: {
+    __dirname: true
+  }
+}
